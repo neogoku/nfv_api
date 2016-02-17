29a1d5ed60c5866a8cf792b861798b0f61064ef7
Umasankari <Umasankari.T@cognizant.com>
2016-02-17 14:42:44 +0530

    Added list_vnf_catalog url

--- api/urls.py
+++ api/urls.py
@@ -5,6 +5,10 @@ from . import views
 urlpatterns = [
     # url(r'^$',views.index,name='index'),
     url(r'^loginHandler/(?P<userId>.+)/(?P<pwd>.+)', views.loginHandler),
+    url(
+        r'^create/(?P<catalogName>.+)/(?P<catalogDesc>.+)/(?P<vmImageFile>.+)/(?P<vnfdFilename>.+)/(?P<vnfdCfgFilename>.+)/(?P<vnfdParamFilename>.+)',
+        views.create_vnf_catalog),
+    url(r'^listCatalog', views.list_vnf_catalog),
     # url(r'^$',views.login,name='login'),
     # url(r'^login/', views.loginHandler),
 ]
